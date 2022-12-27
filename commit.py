ba297bddb1cb9abf4f1c83461e05d2af5e9682a0
brnt-toast <brnt-toast@gmail.com>
2022-12-27 04:50:13 -0800

    removed broken scripts

--- commit.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-import os 
-
-os.system("git add .")
-os.system(f"git commit -m '{input('[+] Message: ')}'")
--- hashthis.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-import sys 
-from hashlib import sha256
-
-print(sha256(sys.argv[1].encode('utf-8')).hexdigest())
