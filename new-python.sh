7c3d74ad1042d98d575a45a79b4d0bdf2d2f9475
brnt-toast <yoyo.beerme@gmail.com>
2023-01-28 22:55:54 -0500

    [+] init: new-python.sh

---
+++ new-python.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+touch rename-me.py
+
+printf  "\
+#!/usr/bin/env python3
+
+\"\"\"
+Module Docstring
+\"\"\"
+
+def main():
+
+    print(\"hello world\")
+
+
+if __name__ == \"__main__\":
+    main()
+" > rename-me.py
