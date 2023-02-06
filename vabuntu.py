08a76549d44a541b0ed837f02a7d9e76d2a2ba1d
brnt-toast <yoyo.beerme@gmail.com>
2023-02-06 14:32:43 -0500

    no more nt systems; no need for python

--- vabuntu.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-command_list = ['vagrant init ubuntu/focal64', # is this PEP 8??
-                'vagrant up', 'vagrant ssh' ]
-
-for command in command_list:
-    os.system(command)
